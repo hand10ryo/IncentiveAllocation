2d982dc33c203a2b57331548aff596e71e44a469
hand10ryo <hand10ryo@yahoo.co.jp>
2023-09-02 22:38:25 +0900

    implement score_curve

--- IncentiveAllocation/Evaluator/BinaryTreatmentEvaluator.py
+++ IncentiveAllocation/Evaluator/BinaryTreatmentEvaluator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class BaseBinaryTreatmentEvaluator:
     def __init__(self, policy, X_test, t_test, y_test, z_test, p_test = None):
@@ -38,6 +39,50 @@ class AddonPerCost(BaseBinaryTreatmentEvaluator):
             return addon / total_cost
         else:
             return 0
+        
+    def score_curve(
+            self, 
+            budget_array: np.ndarray = None, 
+            budget_rate_array: np.ndarray = None, 
+            n_split: int = None,
+            actual_cost: bool = True,
+        ):
+        if self.p_test is None:
+            p_test = self.t_test.mean() * np.ones_like(self.t_test)
+        else:
+            p_test = self.p_test
+
+        pred_dict = self.policy.predict(self.X_test)
+
+        if actual_cost:
+            cost = self.z_test * self.t_test / p_test
+        else:
+            cost = pd.Series(pred_dict["cost"])
+
+        sorted_index = self.policy.sort(self.X_test)
+        cost_cumsum = cost.iloc[sorted_index].cumsum()
+
+        if budget_array is None:
+            if budget_rate_array is None:
+                if n_split is None:
+                    raise ValueError
+                else:
+                    budget_rate_array = np.arange(1, n_split+1) / n_split
+                
+            budget_array =  budget_rate_array * cost_cumsum.max() 
+
+
+        addon_revenue_list = []
+        for budget in budget_array:
+            give_flg = cost_cumsum < budget
+            tg_revenue = self.y_test.loc[give_flg & (self.t_test == 1)].mean()
+            cg_revenue = self.y_test.loc[give_flg & (self.t_test == 0)].mean()
+            tg_cost = self.z_test.loc[give_flg & (self.t_test == 1)].mean()
+            addon_revenue = (tg_revenue - cg_revenue) / tg_cost
+            addon_revenue_list.append(addon_revenue)
+
+        return budget_array, budget_rate_array, np.array(addon_revenue_list)
+
 
 
 class Addon(BaseBinaryTreatmentEvaluator):
